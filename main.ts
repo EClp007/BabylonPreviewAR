1440c21d662cb55b0735df47d223b8618c99ae8d
Erik Cercasov <96184306+EClp007@users.noreply.github.com>
2024-05-12 19:58:42 +0200

    Update main.ts to remove import of "@babylonjs/materials"

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 import * as BABYLON from "@babylonjs/core";
 import "@babylonjs/loaders/glTF";
 import { Inspector } from "@babylonjs/inspector";
-import "@babylonjs/materials";
 
 // Get the canvas element from the DOM
 const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
