d27c7fc4e030c77ef51eac6365288e674efd27a0
Erik Cercasov <96184306+EClp007@users.noreply.github.com>
2024-05-12 18:44:08 +0200

    add teste video

--- main.js
+++ main.js
@@ -42,6 +42,35 @@ const createScene = async () => {
 	model.overlayColor = new BABYLON.Color3(0, 0, 0.5);
 	model.scaling = new BABYLON.Vector3(0.2, 0.2, 0.2);
 
+	const planeOpts = {
+		height: 5.4762,
+		width: 7.3967,
+		sideOrientation: BABYLON.Mesh.DOUBLESIDE,
+	};
+	const ANote0Video = BABYLON.MeshBuilder.CreatePlane(
+		"plane",
+		planeOpts,
+		scene,
+	);
+	const vidPos = new BABYLON.Vector3(0, 0, 0.1);
+	ANote0Video.position = vidPos;
+	const ANote0VideoMat = new BABYLON.StandardMaterial("m", scene);
+	const ANote0VideoVidTex = new BABYLON.VideoTexture(
+		"vidtex",
+		"babylonjs.mp4",
+		scene,
+	);
+	ANote0VideoMat.diffuseTexture = ANote0VideoVidTex;
+	ANote0VideoMat.roughness = 1;
+	ANote0Video.material = ANote0VideoMat;
+	scene.onPointerObservable.add((evt) => {
+		if (evt.pickInfo.pickedMesh === ANote0Video) {
+			if (ANote0VideoVidTex.video.paused) ANote0VideoVidTex.video.play();
+			else ANote0VideoVidTex.video.pause();
+			console.log(ANote0VideoVidTex.video.paused ? "paused" : "playing");
+		}
+	}, BABYLON.PointerEventTypes.POINTERPICK);
+
 	/*
   const ground = new BABYLON.MeshBuilder.CreateGround('ground', {
     height: 100,
@@ -119,7 +148,7 @@ const createScene = async () => {
 			});
 		} else {
 			// vr
-			
+
 			featureManager.enableFeature(
 				BABYLON.WebXRFeatureName.MOVEMENT,
 				"latest",
