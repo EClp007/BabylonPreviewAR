9a36a51a03875feb15418b3b233e1fa70c7c1e8b
Erik Cercasov <96184306+EClp007@users.noreply.github.com>
2024-06-08 16:16:25 +0200

    Remove unused components and update dependencies

--- components/vrMovement.js
+++
@@ -1,5 +0,0 @@
-export const vrMovement = async (featureManager, defaultXRExperience) => {
-	featureManager.enableFeature(BABYLON.WebXRFeatureName.MOVEMENT, "latest", {
-		xrInput: defaultXRExperience.input,
-	});
-};
